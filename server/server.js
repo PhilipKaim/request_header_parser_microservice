5484c1dcd0fe6df5134c06d10073170f7ed6750c
Chillup <philipkaim@gmail.com>
2018-08-31 17:40:45 -0400

    fixed process env port for heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/api/whoami', (req, res) => {
     var ipaddress = req.ip;
