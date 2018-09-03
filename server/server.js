ea4cfdfb74235ed718fb11022810cf81b8ff8963
Chillup <philipkaim@gmail.com>
2018-09-03 14:57:02 -0400

    added view, get clients lang and software

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
 
 app.get('/api/whoami', (req, res) => {
     var ipaddress = req.ip;
-
-    var language = req.header['accept-language'];
-    var software = req.header['user-agent'];
+    var language = req.headers["Accept-Language"];
+    var software = req.headers['User-Agent'];
 
     res.send({ ipaddress, language, software });
 });
